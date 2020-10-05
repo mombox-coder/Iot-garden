94c8c56e419d30cbaee3c970e48afb88a7525056
mombox-coder <72390334+mombox-coder@users.noreply.github.com>
2020-10-05 13:01:13 +0100

    Add files via upload

---
+++ thing_properties.c
@@ -0,0 +1,27 @@
+#include <ArduinoIoTCloud.h>
+#include <WiFiConnectionManager.h>
+#include "arduino_secrets.h"
+
+const char THING_ID[] = "<thing id string here>";
+
+const char SSID[]     = SECRET_SSID;    // Network SSID (name)
+const char PASS[]     = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
+
+void onValveOpenChange();
+
+float humidity;
+float temperature;
+float waterFlow;
+int soilMoisture;
+bool valveOpen;
+
+void initProperties(){
+  ArduinoCloud.setThingId(THING_ID);
+  ArduinoCloud.addProperty(humidity, READ, 10 * SECONDS, NULL);
+  ArduinoCloud.addProperty(temperature, READ, 10 * SECONDS, NULL);
+  ArduinoCloud.addProperty(waterFlow, READ, 1 * SECONDS, NULL);
+  ArduinoCloud.addProperty(soilMoisture, READ, 10 * SECONDS, NULL);
+  ArduinoCloud.addProperty(valveOpen, READWRITE, ON_CHANGE, onValveOpenChange);
+}
+
+ConnectionManager *ArduinoIoTPreferredConnection = new WiFiConnectionManager(SSID, PASS);
